ddd1d12982f2c5b500f77b23b518fa141a0da982
GiovanniAcireale <gio.acireale@gmail.com>
2023-01-21 02:27:42 -0500

    initial commit

---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ModernDesign.Core;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
